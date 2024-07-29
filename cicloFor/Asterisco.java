d8b675fd97df0e910078690e8d8eb59b23416580
karinatabares1 <anakarinatabarescamargo707@gmail.com>
2024-07-28 21:25:42 -0500

    ejercicios

---
+++ cicloFor/Asterisco.java
@@ -0,0 +1,9 @@
+package cicloFor;
+
+public class Asterisco {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.print("*");
+        }
+    }
+}
